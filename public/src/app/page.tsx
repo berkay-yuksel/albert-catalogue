8c521af2f2e83ecca302e3ee3734eab9c80dfec5
berkay-yuksel <98.berkay.yuksel@gmail.com>
2026-08-30 17:16:16 +0300

    updated files

--- public/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { PRODUCTS } from "@/data/products";
-import { CatalogClient } from "@/components/CatalogClient";
-
-export default function Home() {
-  return <CatalogClient products={PRODUCTS} />;
-}
